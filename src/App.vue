db68746d2db8fcb35d4287ad93b7391c81732c22
Yovel Becker <yovelb@misterbit.co.il>
2020-09-18 12:24:01 +0300

    added nav bar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <NavBar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/Common/NavBar'
 import soundService from './services/soundService'
 export default {
-  
+  components:{
+    NavBar
+  }
 }
 </script>
---
+++ src/components/Common/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="nav-bar-container">
+      <h1>Nav Bar</h1>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
