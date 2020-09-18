61ba7e6770f104b5ed064699c4a77731302e292b
Yovel Becker <yovelb@misterbit.co.il>
2020-09-18 15:28:48 +0300

    sound preview shows square with preview name

--- src/components/Sound/SoundPreview.vue
+++ src/components/Sound/SoundPreview.vue
@@ -1,15 +1,37 @@
 <template>
   <section class="sound-preview-container">
-      <h1>Sound Preview</h1>
+    <h4>{{ sound.name | capitalizeAll }}</h4>
   </section>
 </template>
 
 <script>
 export default {
-
+  props: {
+    sound: {
+      type: Object,
+      required: false
+    }
+  },
+  created() {
+    console.log(this.sound)
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.sound-preview-container {
+  width: 15vw;
+  height: 15vw;
+  margin: 20px;
+  padding: 10px 0;
+  border-radius: 5px;
+  box-shadow: 0px 0px 17px 1px rgba(0, 0, 0, 0.3);
+  background-image: linear-gradient(to top, #4481eb 0%, #04befe 100%);
+  cursor: pointer;
+  display: flex;
+  flex-direction: column-reverse;
+  h4 {
+    text-align: center;
+  }
+}
 </style>
