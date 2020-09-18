3cede67029c0210e102b250b4066d602b348fb9d
Yovel Becker <yovelb@misterbit.co.il>
2020-09-18 15:29:06 +0300

    sound list renders a list of given previews

--- src/components/Sound/SoundList.vue
+++ src/components/Sound/SoundList.vue
@@ -1,17 +1,28 @@
 
 <template>
   <section class="sound-list-container">
-    <h1>Sound List</h1>
-    <SoundPreview />
+    <SoundPreview v-for="sound in soundList" :key="sound.id" :sound="sound" />
   </section>
 </template>
 
 <script>
 import SoundPreview from './SoundPreview'
 export default {
+  props: {
+    soundList: {
+      type: Array,
+      required: false
+    }
+  },
   components: { SoundPreview }
 }
 </script>
 
-<style>
+<style lang="scss">
+.sound-list-container{
+  margin: 0 auto;
+  display:flex;
+  flex-wrap: wrap;
+  // justify-content:center;
+}
 </style>
