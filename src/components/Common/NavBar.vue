1806e34371219d894f07dbf07fe6482f1f2a9f9d
Yovel Becker <yovelb@misterbit.co.il>
2020-09-18 15:26:40 +0300

    navbar shows logo

--- src/components/Common/NavBar.vue
+++ src/components/Common/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="nav-bar-container">
-      <h1>Nav Bar</h1>
+    <h1>Power Sounder</h1>
   </section>
 </template>
 
@@ -11,5 +11,17 @@ export default {
 </script>
 
 <style lang="scss">
+@import "@/styles/mixins.scss";
+@import "@/styles/vars.scss";
 
+.nav-bar-container {
+  margin: 20px 0;
+  padding: 20px 0;
+  h1 {
+    @include flex-center;
+    font-size: $font-size-large;
+    letter-spacing: 2px;
+    font-weight: $font-weight-semi-bold;
+  }
+}
 </style>
