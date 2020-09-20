504bfc2415d06c438cd1de5ae0f5526da0a80d42
Yovel Becker <yovelb@misterbit.co.il>
2020-09-21 02:37:54 +0300

    added sound store to app store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import soundStore from './soundStore'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    soundStore
   }
 })
